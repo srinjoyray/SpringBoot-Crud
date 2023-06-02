34ca87d7eea1e067010282b91b29a6ec1a201c6c
Srinjoy Ray <srinjoyray@yahoo.in>
2023-06-02 14:20:33 +0530

    init

---
+++ src/main/java/com/project/crud/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.crud.repository;
+
+import com.project.crud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ src/main/java/com/project/crud/service/UserServiceImplementation.java
@@ -0,0 +1,42 @@
+package com.project.crud.service;
+
+import com.project.crud.model.User;
+import com.project.crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImplementation implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElse(null);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
